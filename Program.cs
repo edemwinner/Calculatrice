f5036c2add8c4f174139b3394e497e420698dfb5
edemwinner <edem.detti@hotmail.fr>
2019-11-03 23:44:22 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Preparatifs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entrez une expression: ");
+            string valeurEtree = Console.ReadLine();
+            Expression expr = new Expression(valeurEtree);
+            var regex = new Regex("(\\+|-|\\*|/)");
+            Console.WriteLine($"La valeur de l'expression est: {expr.doubleValue}");
+        }
+    }
+
+    public class Expression
+    {
+        public string stringValue { get; set; }
+        public double doubleValue
+        {
+            get
+            {
+                return getValue(stringValue);
+            }
+        }
+        public Expression(string expression)
+        {
+            stringValue = expression;
+        }
+        private static double getValue(string stringValue)
+        {
+            double result;
+            try
+            {
+
+                if (stringValue.Contains('('))//une parenthèse ouvrante a été trouvée, on cherche donc sa parenthèse fermante
+                {
+                    var indexDebut = stringValue.LastIndexOf('(');
+                    var indexFin = stringValue.Substring(indexDebut).IndexOf(')');
+                    var regroupement = stringValue.Substring(indexDebut + 1, indexFin - 1);
+                    var tempResult = getValue(regroupement);
+                    var remainExpression = stringValue.Substring(0, indexDebut) + tempResult.ToString() + stringValue.Substring(indexDebut + indexFin + 1);
+                    return getValue(remainExpression);
+
+                }
+                if (stringValue.Contains('*'))
+                {
+                    return getValue(stringValue.EvaluerSelonOperateur('*'));
+                }
+                if (stringValue.Contains('/'))
+                {
+                    return getValue(stringValue.EvaluerSelonOperateur('/'));
+                }
+                if (stringValue.Contains('-'))
+                {
+                    var index = stringValue.IndexOf('-');
+                    if (stringValue.StartsWith('-'))//cas d'un nomnre négatif
+                    {
+                        if (double.TryParse(stringValue, out result))
+                            return result;
+                        else if (stringValue.Contains('+')) // un nombre négatif est additionné à un nomre positif
+                        {
+                            var partieNegative = stringValue.Substring(0, stringValue.IndexOf('+'));
+                            var partiePositive = stringValue.Substring(stringValue.IndexOf('+') + 1);
+                            stringValue = partiePositive + partieNegative;
+                        }
+                        else if (stringValue.Contains('-'))// soustraction d'un nombre à un nombre négatif
+                        {
+                            var indexSoustraction = 1 + stringValue.Substring(1).IndexOf('-');
+                            stringValue = stringValue.Remove(0, 1); //on ignore temporairement le signe négatif
+                            stringValue = stringValue.Insert(indexSoustraction, "+"); // la soustraction devient une addition
+                            stringValue = stringValue.Remove(indexSoustraction - 1, 1); // on supprime le signe de soustraction
+                            return -getValue(stringValue);// on retourne l'opposé de l'addition résultante
+                        }
+                    }
+                    else if (stringValue[index - 1] == '+') //cas d'addition d'un nombre à un nombre négatif:3+-5 par exemple
+                        stringValue = stringValue.Remove(index - 1, 1);//l'opération revient à une simple soustraction
+                    return getValue(stringValue.EvaluerSelonOperateur('-'));
+                }
+                if (stringValue.Contains('+'))
+                {
+                    return getValue(stringValue.EvaluerSelonOperateur('+'));
+                }
+
+                if (double.TryParse(stringValue, out result))
+                    return result;
+                return double.NaN;
+            }
+            catch (Exception e)
+            {
+                return double.NaN;
+            }
+        }
+    }
+
+}
---
+++ Utilitaire.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Preparatifs
+{
+    public static class Utilitaire
+    {
+        public static bool EstOperateur(this char c)
+        {
+            return c == '*' || c == '/' || c == '+' || c == '-';
+        }
+        public static string EvaluerSelonOperateur(this string expression, char operateur)
+        {
+            bool operationtionNegative = false;
+            int index = expression.IndexOf(operateur); // on recherche les opérandes
+            if ((operateur == '*' || operateur == '/' || operateur == '-') && expression[index + 1] == '-')//multiplication ou division avec un nombre négatif sans les parenthèses
+            {
+                operationtionNegative = true;
+                expression = expression.Remove(index + 1, 1); //on ignore temporairement le signe négatif
+            }
+            
+            var operande1Builder = new StringBuilder();
+            var operande2Builder = new StringBuilder();
+            var leftRemainBuilder = new StringBuilder();
+            var rightRemainRemainBuilder = new StringBuilder();
+            bool rechercherOperateur = true;
+            for (var i = index - 1; i >= 0; i--)
+            {
+                var caract = expression[i];
+                if (caract.EstOperateur() || !rechercherOperateur)
+                {
+                    rechercherOperateur = false;
+                    leftRemainBuilder.Insert(0, caract);
+                }
+                else
+                {
+                    operande1Builder.Insert(0, caract);
+                }
+            }
+            rechercherOperateur = true;
+            for (var i = index + 1; i < expression.Length; i++)
+            {
+                var caract = expression[i];
+                if (caract.EstOperateur() || !rechercherOperateur)
+                {
+                    rechercherOperateur = false;
+                    rightRemainRemainBuilder.Append(caract);
+                }
+                else
+                {
+                    operande2Builder.Append(caract);
+                }
+            }
+            string operande1 = operande1Builder.ToString();
+            string operande2 = operande2Builder.ToString();
+            double tempResult = 0;
+            switch (operateur)
+            {
+                case '*':
+                    tempResult = double.Parse(operande1) * double.Parse(operande2);
+                    if (operationtionNegative)
+                        tempResult = -tempResult; //on reconsidère le signe négatif initialement ignoré
+                    break;
+                case '/':
+                    tempResult = double.Parse(operande1) / double.Parse(operande2);
+                    if (operationtionNegative)
+                        tempResult = -tempResult;//on reconsidère le signe négatif initialement ignoré
+                    break;
+                case '+':
+                    tempResult = double.Parse(operande1) + double.Parse(operande2);
+                    break;
+                case '-':
+                    tempResult =  double.Parse(operande1) - double.Parse(operande2);
+                    if (operationtionNegative)
+                        tempResult = -tempResult;//on reconsidère le signe négatif initialement ignoré
+                    break;
+            }
+
+            var resultExpression = leftRemainBuilder.ToString() + tempResult.ToString() + rightRemainRemainBuilder.ToString();
+            return resultExpression;
+        }
+
+        public static string EvaluerRegroupement(this string expression)
+        {
+            var indexDebut = expression.LastIndexOf('(');
+            var indexFin = expression.IndexOf(')');
+            var regroupement = expression.Substring(indexDebut + 1, indexFin - indexDebut - 1);
+
+            return regroupement;
+        }
+    }
+}
